5ef4fceaff4c3f40cf897aa2f2114714de5e05fe
philipheferreira <philipheferreira@gmail.com>
2024-09-28 16:59:09 -0300

    Feat: basic front and javascript adjust calls

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-let nomePrimeiroJogo = document.querySelector(".nomeJogo");
-let descricaoJogoPrimeiroJogo = document.querySelector(".descricaoJogo");
-let anoCriacaoPrimeiroJogo = document.querySelector(".anoCriacao");
-let generoPrimeiroJogo = document.querySelector(".generoJogo");
+let nomePrimeiroJogo = document.querySelector(".nomeJogo1");
+let descricaoJogoPrimeiroJogo = document.querySelector(".descricaoJogo1");
+let anoCriacaoPrimeiroJogo = document.querySelector(".anoCriacao1");
+let generoPrimeiroJogo = document.querySelector(".generoJogo1");
 
+let nomeSegundoJogo = document.querySelector(".nomeJogo2");
+let descricaoJogoSegundoJogo = document.querySelector(".descricaoJogo2");
+let anoCriacaoSegundoJogo = document.querySelector(".anoCriacao2");
+let generoSegundoJogo = document.querySelector(".generoJogo2");
+
+let nomeTerceiroJogo = document.querySelector(".nomeJogo3");
+let descricaoJogoTerceiroJogo = document.querySelector(".descricaoJogo3");
+let anoCriacaoTerceiroJogo = document.querySelector(".anoCriacao3");
+let generoTerceiroJogo = document.querySelector(".generoJogo3");
+
+let nomeQuartoJogo = document.querySelector(".nomeJogo4");
+let descricaoJogoQuartoJogo = document.querySelector(".descricaoJogo4");
+let anoCriacaoQuartoJogo = document.querySelector(".anoCriacao4");
+let generoQuartoJogo = document.querySelector(".generoJogo4");
 
 const getJogosTodosURL = 'http://localhost:5081/api/games/get'
 
@@ -17,16 +31,41 @@ const getTodosJogos = async () => {
 	}
 }
 
-let repasseJogo = (jogo) => {
-	nomePrimeiroJogo.innerHTML = jogo.name;
-	descricaoJogoPrimeiroJogo.innerHTML = jogo.description;
-	anoCriacaoPrimeiroJogo.innerHTML = jogo.dateOfCreation;
-	generoPrimeiroJogo.innerHTML = jogo.genre;
+let repasseJogo = (jogo, numero) => {
+
+	if(numero == 1){
+		nomePrimeiroJogo.innerHTML = jogo.name;
+		descricaoJogoPrimeiroJogo.innerHTML = jogo.description;
+		anoCriacaoPrimeiroJogo.innerHTML = jogo.dateOfCreation;
+		generoPrimeiroJogo.innerHTML = jogo.genre;
+	}else 
+	if(numero == 2){
+		nomeSegundoJogo.innerHTML = jogo.name;
+		descricaoJogoSegundoJogo.innerHTML = jogo.description;
+		anoCriacaoSegundoJogo.innerHTML = jogo.dateOfCreation;
+		generoSegundoJogo.innerHTML = jogo.genre;
+	}else 
+	if(numero == 3){
+		nomeTerceiroJogo.innerHTML = jogo.name;
+		descricaoJogoTerceiroJogo.innerHTML = jogo.description;
+		anoCriacaoTerceiroJogo.innerHTML = jogo.dateOfCreation;
+		generoTerceiroJogo.innerHTML = jogo.genre;
+	}else
+	if(numero == 4){
+		nomeQuartoJogo.innerHTML = jogo.name;
+		descricaoJogoQuartoJogo.innerHTML = jogo.description;
+		anoCriacaoQuartoJogo.innerHTML = jogo.dateOfCreation;
+		generoQuartoJogo.innerHTML = jogo.genre;
+	}
+	
 }
+
 
 (async () => {
 	let chamadaTodosJogos = await getTodosJogos();
-	repasseJogo(chamadaTodosJogos[0]);
-
-	console.log("Só avisando que você conectou com sucesso com a API da super Larissa S2");
+	repasseJogo(chamadaTodosJogos[0], 1);
+	repasseJogo(chamadaTodosJogos[1], 2);
+	repasseJogo(chamadaTodosJogos[2], 3);
+	repasseJogo(chamadaTodosJogos[3], 4);
+	console.log(chamadaTodosJogos);
 })()
