8031b019b736c62cde59a6d9bbbab6a9dc13e632
philipheferreira <philipheferreira@gmail.com>
2024-09-28 17:04:36 -0300

    Feat: add id caracter in informations

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
+let idPrimeiroJogo = document.querySelector(".idJogo1");
 let nomePrimeiroJogo = document.querySelector(".nomeJogo1");
 let descricaoJogoPrimeiroJogo = document.querySelector(".descricaoJogo1");
 let anoCriacaoPrimeiroJogo = document.querySelector(".anoCriacao1");
 let generoPrimeiroJogo = document.querySelector(".generoJogo1");
 
+let idSegundoJogo = document.querySelector(".idJogo2");
 let nomeSegundoJogo = document.querySelector(".nomeJogo2");
 let descricaoJogoSegundoJogo = document.querySelector(".descricaoJogo2");
 let anoCriacaoSegundoJogo = document.querySelector(".anoCriacao2");
 let generoSegundoJogo = document.querySelector(".generoJogo2");
 
+let idTerceiroJogo = document.querySelector(".idJogo3");
 let nomeTerceiroJogo = document.querySelector(".nomeJogo3");
 let descricaoJogoTerceiroJogo = document.querySelector(".descricaoJogo3");
 let anoCriacaoTerceiroJogo = document.querySelector(".anoCriacao3");
 let generoTerceiroJogo = document.querySelector(".generoJogo3");
 
+let idQuartoJogo = document.querySelector(".idJogo4");
 let nomeQuartoJogo = document.querySelector(".nomeJogo4");
 let descricaoJogoQuartoJogo = document.querySelector(".descricaoJogo4");
 let anoCriacaoQuartoJogo = document.querySelector(".anoCriacao4");
@@ -34,24 +38,28 @@ const getTodosJogos = async () => {
 let repasseJogo = (jogo, numero) => {
 
 	if(numero == 1){
+		idPrimeiroJogo.innerHTML = jogo.id
 		nomePrimeiroJogo.innerHTML = jogo.name;
 		descricaoJogoPrimeiroJogo.innerHTML = jogo.description;
 		anoCriacaoPrimeiroJogo.innerHTML = jogo.dateOfCreation;
 		generoPrimeiroJogo.innerHTML = jogo.genre;
 	}else 
 	if(numero == 2){
+		idSegundoJogo.innerHTML = jogo.id
 		nomeSegundoJogo.innerHTML = jogo.name;
 		descricaoJogoSegundoJogo.innerHTML = jogo.description;
 		anoCriacaoSegundoJogo.innerHTML = jogo.dateOfCreation;
 		generoSegundoJogo.innerHTML = jogo.genre;
 	}else 
 	if(numero == 3){
+		idTerceiroJogo.innerHTML = jogo.id
 		nomeTerceiroJogo.innerHTML = jogo.name;
 		descricaoJogoTerceiroJogo.innerHTML = jogo.description;
 		anoCriacaoTerceiroJogo.innerHTML = jogo.dateOfCreation;
 		generoTerceiroJogo.innerHTML = jogo.genre;
 	}else
 	if(numero == 4){
+		idQuartoJogo.innerHTML = jogo.id
 		nomeQuartoJogo.innerHTML = jogo.name;
 		descricaoJogoQuartoJogo.innerHTML = jogo.description;
 		anoCriacaoQuartoJogo.innerHTML = jogo.dateOfCreation;
